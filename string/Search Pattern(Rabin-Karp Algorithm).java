36665ad67a2fa7fad576fc7ceb43a1f6c7e7d382
kseitsu <kseitsu@gmail.com>
2025-05-26 01:19:59 +0530

    Update Search Pattern(Rabin-Karp Algorithm).java

--- string/Search Pattern(Rabin-Karp Algorithm).java
+++ string/Search Pattern(Rabin-Karp Algorithm).java
@@ -1,2 +1,30 @@
  // Search Pattern (Rabin-Karp Algorithm) 
 
+// https://www.geeksforgeeks.org/problems/search-pattern-rabin-karp-algorithm--141631/1
+
+// bruteforce aprpaoch 
+
+ ArrayList<Integer> search(String pattern, String text) {
+        // your code here
+        
+        TreeSet<Integer> st = new TreeSet<>();
+        int n = text.length();
+        int m = pattern.length();
+        
+        int i=0;
+        while(i<n){
+            
+            int idx = text.indexOf(pattern,i);
+            if(idx !=-1){
+                st.add(idx+1);
+            }
+            i++;
+        }
+       
+       ArrayList<Integer> list = new ArrayList<>();
+       list.addAll(st);
+       return list;
+    }
+
+
+//using
